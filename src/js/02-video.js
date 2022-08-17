43d481d533a387f315facd91b41b19c4499600e8
Yurij  Shaklak <yurijfedor178@gmail.com>
2022-08-17 21:25:22 +0300

    add task number 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'videoplayer-current-time';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+uploudPlayer();
+
+player.on('timeupdate', throttle(setLocalStorage, 1000));
+
+function setLocalStorage(curentTime) {
+  const currentTimePlay = curentTime.seconds;
+  localStorage.setItem(STORAGE_KEY, currentTimePlay);
+}
+
+function uploudPlayer() {
+  const timePlay = localStorage.getItem(STORAGE_KEY);
+  if (timePlay) {
+    player.setCurrentTime(timePlay);
+  }
+}
